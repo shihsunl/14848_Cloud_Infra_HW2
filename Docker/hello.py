0feb99ce5c17b0b1d62217faa416a6717a285222
bensonlin321 <bensonlin321@gmail.com>
2021-09-20 18:41:30 -0700

    feat: use kubernetes to run container

--- Docker/hello.py
+++ Docker/hello.py
@@ -1,2 +1,15 @@
 import os
-print("Hello World")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    name = os.environ.get("NAME", "World")
+    return "Hello {}".format(name)
+
+if __name__ == "__main__":
+    print("Hello World")
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
+
